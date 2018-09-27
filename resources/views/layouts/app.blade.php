8f6c4ded7efecae4e084c1801bbf6f8b3263cfc0
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-09-28 03:52:39 +0800

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name', 'GSIS Village School Admin')}}</title>
+    <title>GSIS</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <link rel="stylesheet" href="{{asset('css/dashboard.css')}}" />
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
