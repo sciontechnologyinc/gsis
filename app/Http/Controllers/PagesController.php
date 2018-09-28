0fdca6b3d5714192224e726f8ec99b4c8f616e0a
WAJJ FREELANCE <wajjfreelance@gmail.com>
2018-09-28 02:26:24 -0700

    sa
    sa

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -352,12 +352,13 @@ class PagesController extends Controller
         return view('pages.records')->with($data);
     }
 
-    public function settings() {
+    public function settings(Request $request) {
         $data = array(
             'title' => 'Profile',
             'pages' => $this->pages,
             'user' => Auth::user()
-        );
+        ); 
+
         return view('pages.settings')->with($data);
     }
 
@@ -379,6 +380,24 @@ class PagesController extends Controller
         } else {
             $fileNameToStore = $request->input('current_photo');
         }
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+        }
+ 
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+        }
+ 
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:6|confirmed',
+        ]);
+ 
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
 
         $user = User::find($request->input('my_id'));
         $user->name = $request->input('name');
--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -9,24 +9,40 @@
         {{Form::label('name'), 'Full Name'}}
         {{Form::text('name', $user->name, ['class' => 'form-control', 'placeholder' => 'Full Name'])}}
     </div>
-    <div class="form-group">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
+    <div class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
+                            <label for="new-password" class="new-password">Current Password</label>
+ 
+                          
+                                <input id="current-password" type="password" class="form-control" name="current-password" required>
+ 
+                                @if ($errors->has('current-password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('current-password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        
-
+                            
+                        </div>
+ 
+                        <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
+                            <label for="new-password" class="new-password">New Password</label>
+ 
+                           
+                                <input id="new-password" type="password" class="form-control" name="new-password" required>
+ 
+                                @if ($errors->has('new-password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('new-password') }}</strong>
+                                    </span>
+                                @endif
+                           
+                        </div>
+ 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            <label for="new-password-confirm" class="new-password-confirm">Confirm New Password</label>
+ 
+                           
+                                <input id="new-password-confirm" type="password" class="form-control" name="new-password_confirmation" required>
+                      
                         </div>
 
 
